e16c69d04847d2cc34fc7b144c69d69b6694a3f5
Victor Pires <victorpiressk@gmail.com>
2026-02-06 21:00:29 -0300

    feat: add PostMedia model for multiple media support

--- posts/models/__init__.py
+++ posts/models/__init__.py
@@ -6,6 +6,7 @@ Expõe os models Post, Comment e Like.
 
 from .comment import Comment
 from .like import Like
+from .media import PostMedia 
 from .post import Post
 
-__all__ = ["Post", "Comment", "Like"]
+__all__ = ["Post", "Comment", "Like", "PostMedia"]
