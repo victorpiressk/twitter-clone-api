97106c70f97af43990dad6f5251dfeec558756f9
Victor Pires <victorpiressk@gmail.com>
2026-03-17 02:58:39 -0300

    feat(users): add update_account and change_password endpoints

--- users/views/user_viewset.py
+++ users/views/user_viewset.py
@@ -2,14 +2,19 @@
 User ViewSet.
 """
 
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from users.models import User
 from users.permissions import IsOwnerOrReadOnly
-from users.serializers import UserCreateSerializer, UserSerializer
+from users.serializers import (
+    ChangePasswordSerializer,
+    UserAccountSerializer,
+    UserCreateSerializer,
+    UserSerializer,
+)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -39,6 +44,22 @@ class UserViewSet(viewsets.ModelViewSet):
         if self.action in ["list", "retrieve", "followers", "following"]:
             return [AllowAny()]
         if self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return [IsAuthenticated()]
+    
+    def get_serializer_class(self):
+        if self.action == "create":
+            return UserCreateSerializer
+        if self.action == "update_account":
+            return UserAccountSerializer
+        return UserSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [AllowAny()]
+        if self.action in ["list", "retrieve", "followers", "following"]:
+            return [AllowAny()]
+        if self.action in ["update", "partial_update", "destroy", "update_account", "change_password"]:
             return [IsAuthenticated(), IsOwnerOrReadOnly()]
         return [IsAuthenticated()]
 
@@ -71,3 +92,34 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(following_users, many=True)
 
         return Response(serializer.data)
+
+    @action(detail=True, methods=["patch"], url_path="account")
+    def update_account(self, request, pk=None):
+        """
+        Atualiza dados sensíveis da conta (email, phone, username).
+        Requer senha atual para confirmar.
+        """
+        user = self.get_object()
+        serializer = UserAccountSerializer(
+            user, data=request.data, partial=True, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(UserSerializer(user, context={"request": request}).data)
+
+    @action(detail=True, methods=["post"], url_path="change-password")
+    def change_password(self, request, pk=None):
+        """
+        Altera a senha do usuário.
+        Requer senha atual + nova senha + confirmação.
+        """
+        self.get_object()  # garante permissão IsOwnerOrReadOnly
+        serializer = ChangePasswordSerializer(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            {"detail": "Senha alterada com sucesso."},
+            status=status.HTTP_200_OK,
+        )
